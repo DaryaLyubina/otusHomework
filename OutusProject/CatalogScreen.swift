d5e09a092a85812ee6cda3a93de97c303dd929c6
Darya <daria.klochkova16@gmail.com>
2022-03-30 10:52:44 +0300

    add navigation view and naLinks

--- OutusProject/CatalogScreen.swift
+++ OutusProject/CatalogScreen.swift
@@ -10,12 +10,37 @@ import SwiftUI
 struct CatalogScreen: View {
     var body: some View {
         
-        Text("CatalogScreen")
+        NavigationView {
+            List {
+                NavigationLink(destination: ListDestinationScreen()) {
+                    Label("Documents", systemImage: "folder")
+                }
+                
+                NavigationLink(destination: ListDestinationScreen()) {
+                    Label("Downloads", systemImage: "folder")
+                }
+                
+                NavigationLink(destination: ListDestinationScreen()) {
+                    Label("My Folder", systemImage: "folder")
+                }
+                
+                NavigationLink(destination: ListDestinationScreen()) {
+                    Label("Bin", systemImage: "folder")
+                }
+            }
+        }.navigationTitle("list")
     }
 }
 
 struct CatalogScreen_Previews: PreviewProvider {
     static var previews: some View {
         CatalogScreen()
+    }
+}
+
+
+struct ListDestinationScreen: View {
+    var body: some View {
+        Text("ListDestinationScreen")
     }
 }
